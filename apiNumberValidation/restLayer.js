02850997852afac01d219ff29203c17905ea0fd1
unknown <elisandro.santolin@outlook.com>
2023-05-06 11:45:31 -0300

    Feat: added API-REST validation phone number

---
+++ apiNumberValidation/restLayer.js
@@ -0,0 +1,3 @@
+const express = require('express');
+const validationApp = express();
+const validationPhone = require('.') 
---
+++ apiNumberValidation/serviceLayer.js
@@ -0,0 +1,5 @@
+function validationPhone(phone);{
+    let validation = phone.length == 15  true : false;
+    return validation;
+}
+exports.validationPhone = validationPhone;
