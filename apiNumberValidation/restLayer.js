1b7460d175dd3932885d52683154ad74489b8f53
unknown <elisandro.santolin@outlook.com>
2023-05-07 10:24:37 -0300

    Refactor: implemented function validation phone size

--- apiNumberValidation/restLayer.js
+++ apiNumberValidation/restLayer.js
@@ -1,3 +1,15 @@
 const express = require('express');
 const validationApp = express();
-const validationPhone = require('.') 
+const validationPhone = require('./serviceLayer');
+
+validationApp.get('/', (req, res) => {
+    let phone = req.query.phone;
+
+    let validation = validationPhone.validation(phone);
+    res.json({valid: validation});
+});
+
+validationApp.listen(8080, () => {
+    let data = new Date();
+    console.log('Server initial date: '+data);
+});
--- apiNumberValidation/serviceLayer.js
+++ apiNumberValidation/serviceLayer.js
@@ -1,5 +1,5 @@
-function validationPhone(phone);{
-    let validation = phone.length == 15  true : false;
+function validationPhone(phone){
+    let validation = phone.length == 15  ? true : false;
     return validation;
 }
-exports.validationPhone = validationPhone;
+exports.validationPhone = validationPhone; 
